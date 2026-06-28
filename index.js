2f809f9ef4fc672ebc054f8ae48c3f4f4fa1e8fa
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-06-28 07:31:40 +0000

    Enable QR code printing in terminal for socket
    Added printQRInTerminal option to makeWASocket.

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const PHONE_NUMBER = process.env.PHONE_NUMBER?.trim();
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
     const { version } = await fetchLatestBaileysVersion();
-    const sock = makeWASocket({ version, auth: state, logger: pino({ level: 'silent' }) });
-
+   const sock = makeWASocket({ 
+  version, 
+  auth: state, 
+  printQRInTerminal: true, // <--- C’EST ÇA LE TRUC
+  logger: pino({ level: 'silent' }) 
+});
     if (!state.creds.registered) {
         console.log('Attente 5s avant de demander le code...');
         await new Promise(r => setTimeout(r, 5000));
